944f86ffef8be680f16a18b3ae24f66b0c826a9c
SirAuzer <cookooc017@gmail.com>
2021-03-12 20:27:04 +0200

    Harry

--- src/tes/edu/modul1/Harry.java
+++ src/tes/edu/modul1/Harry.java
@@ -3,6 +3,7 @@ package tes.edu.modul1;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 /**
  * @author Arsenii
@@ -13,33 +14,16 @@ import java.nio.file.Paths;
  **/
 
 public class Harry {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException
+    {////////download a text
         String text = new String(Files.readAllBytes(Paths.get("C:\\Users\\Hades\\Desktop\\harry.txt")));
-        System.out.println(text.substring(0, 100));
-        text = text.replaceAll("[^A-Za-z ']", "");
+        text = text.replaceAll("[^A-Za-z ']", "");/// Clean the words from a punctuation signs
 
-        String[] words = text.split(" +");
-        System.out.println(words.length);
-
-        for (int i = 0; i < 100; i++) {
-            System.out.println(words[i]);
-        }
-        System.out.println("------------------------------------");
-
-        int counter = 0;
-        for (int i = 0; i < words.length; i++) {
-            if (words[i].length() == 12)
-            {
-                System.out.println(words[i]);
-                counter++;
-            }
-        }
-        System.out.println(counter);
-        System.out.println("------------------------------------");
+        String[] words = text.split(" +");// Split into an array of words
 
         String stringOfDistictinct ="";
-
-        for (int i = 0; i < words.length; i++) {
+        for (int i = 0; i < words.length; i++)
+        {
             if (!stringOfDistictinct.contains(words[i]))
             {
                 stringOfDistictinct += words[i] + " ";
@@ -47,10 +31,122 @@ public class Harry {
         }
         String[] distincts = stringOfDistictinct.split(" ");
 
-        //Arrays.sort(distincts);
-        System.out.println(distincts.length);
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < distincts.length; i++)
+        {
+            if (distincts[i].startsWith("'"))
+            {
+                distincts[i] = distincts[i].substring(1);
+            }
+        }
+
+        Arrays.sort(distincts); ////Order the words in alphabetic order
+        for (int i = 0; i < 100; i++)
+        {
             System.out.println(distincts[i]);
         }
     }
 }
+
+
+/*
+        AAAAAAAAAARGH
+        ALBUS
+        ALL
+        ALLEY
+        ALLOWED
+        AM
+        AND
+        ANYTHING
+        AT
+        Aaah
+        Aargh
+        Abbott
+        Abou'
+        About
+        Absolutely
+        According
+        Adalbert
+        Add
+        Adrian
+        Africa
+        African
+        After
+        Against
+        Ages
+        Agrippa
+        Ah
+        Aha
+        Ahead
+        Ahem
+        Ahern
+        Alas
+        Alberic
+        Albus
+        Algie
+        Alicia
+        All
+        Alley
+        Alohomora
+        Although
+        Always
+        Am
+        An'
+        And
+        Angelina
+        Another
+        AntiCheating
+        AntiDark
+        Anyone
+        Anythin'
+        Anything
+        Anyway
+        Apothecary
+        Are
+        Aren't
+        Argus
+        Around
+        Arsenius
+        Arts
+        As
+        Asked
+        Astronomy
+        At
+        Ate
+        Atta
+        August
+        Aunt
+        Auntie
+        BC
+        BED
+        BEHIND
+        BELIEVE
+        BOOKS
+        BOOM
+        BOY
+        BREAKIN
+        BROOMSTICKS
+        Back
+        Bad
+        Bagshot
+        Bane
+        Bane'll
+        Barking
+        Barn
+        Baron'll
+        Baron's
+        Barrels
+        Bars
+        Baruffio
+        Bathilda
+        Bbbut
+        Bbut
+        Beans
+        Beasts
+        Beaters
+        Beats
+        Because
+        Bedroom
+        Beechwood
+        Before
+        Befuddle
+*/
