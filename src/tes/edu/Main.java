87630a85359ddb023b5454361fcfa81aca67f6f7
SirAuzer <cookooc017@gmail.com>
2021-03-12 17:53:16 +0200

    Initial commit

---
+++ src/tes/edu/Main.java
@@ -0,0 +1,8 @@
+package tes.edu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Somebody once told me");
+    }
+}
---
+++ src/tes/edu/modul1/AboutMe.java
@@ -0,0 +1,38 @@
+package tes.edu.modul1;
+
+/**
+ * @author Arsenii
+ * @version 1.0.0
+ * @project vsem
+ * @class AboutMe
+ * @since 12.03.2021 - 00.55
+ **/
+
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("Hello..");
+        byte myAge = 21;
+        System.out.println(myAge);
+
+        double myHeight = 178.5;
+        System.out.println(myHeight);
+
+        float myWight = 75.35f;
+        System.out.println(myWight);
+
+        char myGender = 'M';
+        System.out.println(myGender);
+
+        short sleepTime = 3;
+        System.out.println(sleepTime);
+
+        long timeAgo = 7_000_000_000L;
+        System.out.println(timeAgo);
+
+        int musicTime = 24;
+        System.out.println(musicTime);
+
+        boolean check = true;
+        System.out.println(check);
+    }
+}
