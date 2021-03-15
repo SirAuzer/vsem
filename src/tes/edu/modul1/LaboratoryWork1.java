5cbe84dc9e876f6ec7db34b742f4418f5dc615d8
SirAuzer <cookooc017@gmail.com>
2021-03-15 19:56:27 +0200

    finish

--- src/tes/edu/modul1/LaboratoryWork1.java
+++ src/tes/edu/modul1/LaboratoryWork1.java
@@ -21,22 +21,20 @@ public class LaboratoryWork1 {
     public static void main(String[] args) throws IOException {
         String file = "C:\\Users\\Hades\\Desktop\\harry.txt";
         String textExample = new String(Files.readAllBytes(Paths.get(file)));
-        textExample = textExample.toLowerCase().replaceAll("[^A-Za-z ']", "");
 
         String[] lines = textExample.split("\\n");// Split LINES
         int linesWithHarry = 0;
         for (int i = 0; i < lines.length; i++) {
-            if (lines[i].contains("harry")) linesWithHarry ++;
+            if (lines[i].contains("Harry")) linesWithHarry ++;
         }
         System.out.println("Lines with the word \"Harry\" - " + linesWithHarry);
 
+        textExample = textExample.toLowerCase().replaceAll("[^A-Za-z ']", "");
         String[] words = textExample.split(" +");// Split into an array of words
         String wordWithMaxLength = "";///////1----------
         Arrays.sort(words, Comparator.comparing(String::length, Comparator.reverseOrder()));
         wordWithMaxLength = words[0].length() > wordWithMaxLength.length() ? words[0] : wordWithMaxLength;
-
-        System.out.println(wordWithMaxLength);
-
+        System.out.println("longest word - " + wordWithMaxLength);
 
         int wordBeginFromC = 0;///////////-----3---4
         String stringOfDistictinct ="";
@@ -54,17 +52,18 @@ public class LaboratoryWork1 {
             arrayHashCode[i] = distincts[i].hashCode();
         }
 
-
         Arrays.sort(arrayHashCode);
         int intersectionsOfHashes = 0;
         for (int i = 0; i < arrayHashCode.length - 2; i++) { // 6-- intersections of hashes
             intersectionsOfHashes += (arrayHashCode[i] == arrayHashCode[i+1] & arrayHashCode[i+1] != arrayHashCode[i+2] ) ? 1 : 0;
         }
         System.out.println("count the intersections of hashes - " + intersectionsOfHashes);
-
-
-
-
-
     }
 }
+/*
+
+        Lines with the word "Harry" - 1084
+        longest word - interestinglooking
+        distinct words begin from the letter "C" -339
+        count the intersections of hashes - 3
+        */
