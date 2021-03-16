fc770753131d89a994069e35fae892c2ef181907
SirAuzer <cookooc017@gmail.com>
2021-03-17 01:45:29 +0200

    finished

--- src/tes/edu/modul1/LaboratoryWork1.java
+++ src/tes/edu/modul1/LaboratoryWork1.java
@@ -6,7 +6,6 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
 
 
 /**
@@ -25,37 +24,41 @@ public class LaboratoryWork1 {
         String[] lines = textExample.split("\\n");// Split LINES
         int linesWithHarry = 0;
         for (int i = 0; i < lines.length; i++) {
-            if (lines[i].contains("Harry")) linesWithHarry ++;
+            if (lines[i].contains("Harry")) linesWithHarry++;
         }
         System.out.println("Lines with the word \"Harry\" - " + linesWithHarry);
 
-        textExample = textExample.toLowerCase().replaceAll("[^A-Za-z ']", "");
+        textExample = textExample.replaceAll("[^A-Za-z ']", "");
         String[] words = textExample.split(" +");// Split into an array of words
+
         String wordWithMaxLength = "";///////1----------
         Arrays.sort(words, Comparator.comparing(String::length, Comparator.reverseOrder()));
         wordWithMaxLength = words[0].length() > wordWithMaxLength.length() ? words[0] : wordWithMaxLength;
         System.out.println("longest word - " + wordWithMaxLength);
 
         int wordBeginFromC = 0;///////////-----3---4
-        String stringOfDistictinct ="";
-        for (int i = 0; i < words.length; i++){         //Take  the array of distinct words
-            if (!stringOfDistictinct.contains(words[i])){
-                stringOfDistictinct += (words[i].startsWith("'")) ? words[i].substring(1) + " " : words[i] + " " ;
-                if (words[i].startsWith("c")) wordBeginFromC += 1;//How many distinct words begin from the letter "C"
+        String stringOfDistinct = "";
+        for (int i = 0; i < words.length; i++) {         //Take  the array of distinct words
+
+            if (!stringOfDistinct.contains(words[i])) {
+                stringOfDistinct += words[i] + " ";
             }
+            if (words[i].toLowerCase().startsWith("'c") || words[i].toLowerCase().startsWith("c")) {//How many distinct words begin from the letter "C"
+                wordBeginFromC++;
+            }
+
         }
         System.out.println("distinct words begin from the letter \"C\" -" + wordBeginFromC);
-        String[] distincts = stringOfDistictinct.split(" ");
+        String[] distincts = stringOfDistinct.split(" ");
 
-        int [] arrayHashCode = new int[distincts.length]; // --5-- create an array of hashes
+        int[] arrayHashCode = new int[distincts.length]; // --5-- create an array of hashes
         for (int i = 0; i < distincts.length; i++) {
             arrayHashCode[i] = distincts[i].hashCode();
         }
-
         Arrays.sort(arrayHashCode);
         int intersectionsOfHashes = 0;
-        for (int i = 0; i < arrayHashCode.length - 2; i++) { // 6-- intersections of hashes
-            intersectionsOfHashes += (arrayHashCode[i] == arrayHashCode[i+1] & arrayHashCode[i+1] != arrayHashCode[i+2] ) ? 1 : 0;
+        for (int i = 0; i < arrayHashCode.length - 1; i++) { // 6-- intersections of hashes
+            if (arrayHashCode[i] == arrayHashCode[i + 1])intersectionsOfHashes++;
         }
         System.out.println("count the intersections of hashes - " + intersectionsOfHashes);
     }
@@ -64,6 +67,6 @@ public class LaboratoryWork1 {
 
         Lines with the word "Harry" - 1084
         longest word - interestinglooking
-        distinct words begin from the letter "C" -339
-        count the intersections of hashes - 3
+        distinct words begin from the letter "C" -2553
+        count the intersections of hashes - 0
         */
