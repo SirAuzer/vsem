03251f7abde0ec8ad921733d0b2089900c01c35c
SirAuzer <cookooc017@gmail.com>
2021-04-08 01:10:06 +0300

    MIn

---
+++ src/tes/edu/module3/laboratoryWork2/MinSubscribtion.java
@@ -0,0 +1,12 @@
+package tes.edu.module3.laboratoryWork2;
+
+/**
+ * @author Hades
+ * @version 1.0.0
+ * @project vsem
+ * @class MinSubscribtion
+ * @since 08.04.2021 - 01.09
+ **/
+
+public class MinSubscribtion {
+}
