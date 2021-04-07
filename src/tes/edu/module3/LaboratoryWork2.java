bac87c1ce8d8a3fc02cff8fa64cc2d6d52b12981
SirAuzer <cookooc017@gmail.com>
2021-04-07 22:10:44 +0300

    Tes

---
+++ src/tes/edu/module3/LaboratoryWork2.java
@@ -0,0 +1,12 @@
+package tes.edu.module3;
+
+/**
+ * @author Hades
+ * @version 1.0.0
+ * @project vsem
+ * @class LaboratoryWork2
+ * @since 07.04.2021 - 22.09
+ **/
+
+public class LaboratoryWork2 {
+}
