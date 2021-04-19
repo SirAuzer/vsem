e9e9a1c4d5cc064d266467a585c301da6081843d
SirAuzer <cookooc017@gmail.com>
2021-04-19 21:56:35 +0300

    final prefix

--- src/tes/edu/finalTesting/CommonPrefix.java
+++ src/tes/edu/finalTesting/CommonPrefix.java
@@ -16,39 +16,7 @@ public class CommonPrefix {
         if (array.length == 1) {
             return array[1];
         }
-       /* String commonPrefix="";
-        int count = 0;
-        int symbol = 0;
-        if(array.length>0)
-        {
-            for(int i = 0; i < array[0].length(); i++)
-            {
-                int j = 1;
-                while(j < array.length)
-                {
-                    if(array[0].charAt(symbol) == array[j].charAt(symbol))
-                    {
-                        count++;
-                        j++;
-                    }
-                    else
-                        break;
-                }
-                if(count==array.length-1)
-                {
-                    commonPrefix += array[0].charAt(symbol);
-                    count=0;
-                    symbol++;
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
 
-        return "Common Prefix: " + commonPrefix;
-    }*/
         int minLenght = Arrays.stream(array)
                 .mapToInt(String::length).min().orElse(0);
         String minWord = Arrays.stream(array)
