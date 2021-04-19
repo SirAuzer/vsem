c21fc6406256057b77ca77b9ef36238cdcace824
SirAuzer <cookooc017@gmail.com>
2021-04-19 21:55:01 +0300

    final prefix

--- src/tes/edu/finalTesting/CommonPrefix.java
+++ src/tes/edu/finalTesting/CommonPrefix.java
@@ -1,5 +1,8 @@
 package tes.edu.finalTesting;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @author Arsenii
  * @version 1.0.0
@@ -10,10 +13,10 @@ package tes.edu.finalTesting;
 
 public class CommonPrefix {
     public static String prefix(String[] array) {
-        if(array.length == 1){
+        if (array.length == 1) {
             return array[1];
         }
-        String commonPrefix="";
+       /* String commonPrefix="";
         int count = 0;
         int symbol = 0;
         if(array.length>0)
@@ -45,6 +48,25 @@ public class CommonPrefix {
         }
 
         return "Common Prefix: " + commonPrefix;
+    }*/
+        int minLenght = Arrays.stream(array)
+                .mapToInt(String::length).min().orElse(0);
+        String minWord = Arrays.stream(array)
+                .min(Comparator.comparing(String::length)).orElse("");
+        String prefix = "";
+        for (int i = 0; i < array.length;){
+            if(array[i].contains(minWord))
+            {
+                i++;
+                prefix = minWord;
+                continue;
+            }
+            else{
+                minWord = minWord.substring(0, minWord.length()-1);
+            }
+            if(minWord.length()<=0)return null;
+        }
+        return prefix;
     }
 
     public static void main(String[] args) {
@@ -55,7 +77,7 @@ public class CommonPrefix {
         String[] array2 = {"abc", "abcd", "abce", "abcac"};
 
         System.out.println(prefix(array2)); // abc
-        String[] array3 = {"abc", "abcd", "abce"} ;
+        String[] array3 = {"abc", "abcd", "a"} ;
         System.out.println(prefix(array3));
     }
 }
